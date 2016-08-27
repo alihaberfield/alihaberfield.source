3d2f03e2ec75c8463e7e94e587b542cfa7fad727
Ali Haberfield <alison@proactivehealth.se>
2016-08-28 00:56:55 +0200

    Scripts to embed instagram feed

---
+++ js/feed-embed.js
@@ -0,0 +1,12 @@
+var feed = new Instafeed({
+  get: 'user',
+  userId: '2048414042',
+  clientId: '6d1d890fd38e403dac716652aecb57eb',
+  accessToken: '2048414042.6d1d890.fd778857b2d0460299c3d55c49cc9d01',
+  resolution: 'standard_resolution',
+  template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
+  sortBy: 'most-recent',
+  limit: 5,
+  links: true
+});
+feed.run();
