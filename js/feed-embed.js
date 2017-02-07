3da8a0e7de06639df0e43807fe5e56e73e435cfe
Ali Haberfield <alison@proactivehealth.se>
2017-02-07 20:27:36 +0100

    Exclude specific photo IDs from instagram feed

--- js/feed-embed.js
+++ js/feed-embed.js
@@ -1,3 +1,7 @@
+var rejects           = ['1442796174989608823_2048414042', '1440206917909479033_2048414042'];
+var total             = 0;
+var total_to_display  = 6;
+
 var feed = new Instafeed({
   get: 'user',
   userId: '2048414042',
@@ -6,7 +10,17 @@ var feed = new Instafeed({
   resolution: 'standard_resolution',
   template: '<a href="{{link}}" target="_blank" rel="noopener noreferrer" id="{{id}}"><img src="{{image}}" /></a>',
   sortBy: 'most-recent',
-  limit: 6,
-  links: true
+  limit: 12,
+  links: true,
+  success: function() {
+    total = 0;
+  },
+  filter: function(image) {
+    if (rejects.indexOf(image.id) == -1 && total < total_to_display) {
+      total = total + 1;
+      return true;
+    }
+    return false;
+  }
 });
 feed.run();
