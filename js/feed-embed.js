476d89d337fced0a27f5ff9e0beacb344bef0c86
Ali Haberfield <alison@proactivehealth.se>
2017-02-08 12:47:03 +0100

    Revert attempt to hide instagram key

--- js/feed-embed.js
+++ js/feed-embed.js
@@ -1,6 +1,3 @@
----
----
-
 var rejects           = ['1442796174989608823_2048414042', '1440206917909479033_2048414042'];
 var total             = 0;
 var total_to_display  = 6;
@@ -8,8 +5,8 @@ var total_to_display  = 6;
 var feed = new Instafeed({
   get: 'user',
   userId: '2048414042',
-  clientId: '{{ site.instagram_id }}',
-  accessToken: '{{ site.instagram_key }}',
+  clientId: '6d1d890fd38e403dac716652aecb57eb',
+  accessToken: '2048414042.6d1d890.fd778857b2d0460299c3d55c49cc9d01',
   resolution: 'standard_resolution',
   template: '<a href="{{link}}" target="_blank" rel="noopener noreferrer" id="{{id}}"><img src="{{image}}" /></a>',
   sortBy: 'most-recent',
